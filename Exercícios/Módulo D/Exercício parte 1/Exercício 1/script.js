eae1ec88652f05ba377f63656ee488be309aed57
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-03 18:12:34 -0300

    Módulo D Exercício (Parte 1)
    Exercício 1

---
+++ Exercícios/Módulo D/Exercício parte 1/Exercício 1/script.js
@@ -0,0 +1,10 @@
+var hora = 9
+var frase = window.document.getElementById('frase')
+var img = window.document.getElementById('img')
+
+frase.innerHTML = `Agora são ${hora} horas.`
+if (hora > 5 || hora < 12){
+    <style>
+        
+    </style>
+}
