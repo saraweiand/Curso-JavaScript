7eba5c4ff7ea15c3da743f7a3754b7875431f5db
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-04 10:16:17 -0300

    Módulo D Exercício 2
    HTML finalizado
    CSS finalizado
    JS necessário inserir as fotos

--- Exercícios/Módulo D/Exercício 2/script.js
+++ Exercícios/Módulo D/Exercício 2/script.js
@@ -9,7 +9,36 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('sexo')
         var idade = ano - Number(fano.value)
-        if (fsex == 'fem')
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10 ){
+                //CRIANÇA
+            } else if (idade < 21){
+                //JOVEM
+            } else if (idade < 60){
+                //ADULTO
+                img.setAttribute('src', 'homem.jpg')
+            } else{
+                //IDOSO
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10 ){
+                //CRIANÇA
+            } else if (idade < 21){
+                //JOVEM
+            } else if (idade < 60){
+                //ADULTO
+            } else{
+                //IDOSO
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Você é um(a) ${genero} de ${idade} anos.`
+        res.appendChild(img)
     }
 }
 
