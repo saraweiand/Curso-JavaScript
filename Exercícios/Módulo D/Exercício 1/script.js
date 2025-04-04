1ebf6c9e87964a83bdf799158407bee43d0287c1
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-03 21:25:20 -0300

    Módulo D Exercício 1

---
+++ Exercícios/Módulo D/Exercício 1/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var frase = window.document.getElementById('frase')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 1
+    frase.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 5 && hora < 12) {
+        img.src = 'manha.jpg'
+        document.body.style.background = '#455025'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#F2AD6A'
+    } else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#0D2748'
+    }
+}
