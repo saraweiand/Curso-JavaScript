cc4b02e3420abe6591d2c3b905403b69f9beea28
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-07 18:34:29 -0300

    Projeto "Verificador de idade" finalizado

--- Exercícios/Módulo D/Exercício 2/script.js
+++ Exercícios/Módulo D/Exercício 2/script.js
@@ -16,25 +16,31 @@ function verificar() {
             genero = 'Homem'
             if(idade >= 0 && idade < 10 ){
                 //CRIANÇA
+                img.setAttribute('src', 'crianca-h.jpg')
             } else if (idade < 21){
                 //JOVEM
+                img.setAttribute('src', 'jovem-h.jpg')
             } else if (idade < 60){
                 //ADULTO
                 img.setAttribute('src', 'homem.jpg')
             } else{
                 //IDOSO
+                img.setAttribute('src', 'idoso-h.jpg')
             }
         } else if (fsex[1].checked){
             genero = 'Mulher'
             if(idade >= 0 && idade < 10 ){
                 //CRIANÇA
+                img.setAttribute('src', 'crianca-m.jpg')
             } else if (idade < 21){
                 //JOVEM
+                img.setAttribute('src', 'jovem-m.jpg')
             } else if (idade < 60){
                 //ADULTO
                 img.setAttribute('src', 'mulher.jpg')
             } else{
                 //IDOSO
+                img.setAttribute('src', 'idosa-m.jpg')
             }
         }
         res.style.textAlign = 'center'
