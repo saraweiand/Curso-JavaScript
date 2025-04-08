99eee27c18674c3f2799cea7f328f78707c5aa33
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-08 10:07:09 -0300

    Exercício finalizado

--- Exercícios/Módulo E/Repetições/Exercício 1/contador.js
+++ Exercícios/Módulo E/Repetições/Exercício 1/contador.js
@@ -2,14 +2,25 @@ function contar() {
     var in1 = window.document.getElementById('in1')
     var in2 = window.document.getElementById('in2')
     var ipasso = window.document.getElementById('ipasso')
-    var res = window.document.getElementById('res')
+    var resElemento = window.document.getElementById('res')
 
     if (in1.value.length == 0 || in2.value.length == 0 || ipasso.value.length == 0) {
         window.alert('[ERRO] - Digite os dados corretamente')
     } else {
-        while (in1 <= in2) {
-            in1 += ipasso
-           res.innerHTML = `${in1}`
+        var in1 = Number(in1.value)
+        var in2 = Number(in2.value)
+        var ipasso = Number(ipasso.value)
+        var resTexto = " "
+        
+        if (in1 <= in2){
+            for(var i = in1; i <= in2; i += ipasso){
+                resTexto += i + " 👉 "
+            }
+        } else {
+            for(var i = in1; i >= in2; i -= ipasso){
+                resTexto += i + " 👈 "
+            }
         }
+        resElemento.innerHTML = resTexto;
     }
 }
