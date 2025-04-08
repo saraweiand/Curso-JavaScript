f0bd0dff95735f555b04477c64fb92119791193b
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-07 21:02:21 -0300

    While part 1

---
+++ Módulo E/aula13/ambiente.js
@@ -0,0 +1,18 @@
+/* 
+
+Estrutura de repetição com teste lógico no INÍCIO
+
+var c = 1
+while (c <= 6){
+    console.log('Tudo bem?')
+    c++
+}
+    
+*/
+
+// Estrutura de repetição com teste lógico no FINAL
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
