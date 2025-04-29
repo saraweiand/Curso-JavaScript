7ba70f6c20cefc3523b4b507104e5b00c314e81f
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-04-29 09:45:32 -0300

    Módulo F

--- Exercícios/Módulo F/Exercício 1/ambiente.js
+++ Exercícios/Módulo F/Exercício 1/ambiente.js
@@ -1,14 +1,13 @@
 function adicionar(){
     let in1 = document.getElementById('in1')
-    let res = document.getElementById('res')
+    let saida = document.getElementById('saida')
 
     if (in1.value == 0 || in1.value.length == '' ){
         window.alert('ERRO - Insira os dados corretamente')
     } else {
         let in1 = Number(in1.value)
-        let saida = " "
-        res.innerHTML = "";
-
+        let res = " "
+        saida.innerHTML += `Valor ${in1} adicionado.`;
         
     }
 }
